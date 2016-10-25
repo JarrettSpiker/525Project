ff86e30d7d9351ea69e59d9698acb1273f296857
Jarrett Spiker <jspiker@synopsys.com>
2016-10-25 13:51:57 -0600

    add some extra UI to the Access Controller

--- AccessControlSystem/app/src/main/java/com/jspiker/accesscontrolsystem/ReinitializeActivity.java
+++ AccessControlSystem/app/src/main/java/com/jspiker/accesscontrolsystem/ReinitializeActivity.java
@@ -5,33 +5,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class ReinitializeActivity extends AppCompatActivity {
 
-    Spinner numDevicesSpinner;
+    private static final String numberOfDevicesFound = "Number of devices found: ";
 
-    int numDevices = 0;
+    private Spinner numDevicesSpinner;
+    private Switch requirePasscodeSwitch;
+
+    private int numDevices = 0;
+    private int foundSoFar = 0;
+    private boolean requirePasscode = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reinitialize);
 
+        requirePasscodeSwitch = (Switch) findViewById(R.id.requirePasscodeSwitch);
         numDevicesSpinner = (Spinner) findViewById(R.id.numItemsSpinner);
         Button confirmButton = (Button) findViewById(R.id.confirmNumDevicesButton);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 numDevices = numDevicesSpinner.getSelectedItemPosition()+1;
+                requirePasscode = requirePasscodeSwitch.isChecked();
+                requirePasscodeSwitch.setEnabled(false);
                 numDevicesSpinner.setEnabled(false);
                 tryToFindDevices();
             }
         });
     }
 
-    public void tryToFindDevices(){
+    private void tryToFindDevices(){
         TextView text = (TextView) findViewById(R.id.numDevicesText);
-        text.setText("Number of devices found: 0");
+        text.setText(numberOfDevicesFound + foundSoFar);
         //TODO try to find devices
     }
 }
